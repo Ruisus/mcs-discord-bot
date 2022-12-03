c3f26f0a5c3502d018c713c64c056fea10b6df80
Ruisus <lfsoto03@gmail.com>
2022-12-02 23:16:35 -0500

    Online Status and update Component handler

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,8 +1,12 @@
+const Discord = require("discord.js");
+const { ActivityType } = require('discord.js');
+
 module.exports = {
     name: 'ready',
     once: true,
     async execute(client){
         console.log(`${client.user.tag} is now logged in and online.`);
+        client.user.setActivity('MPS 23 | REBORN', { type: ActivityType.Playing });
         client.application.commands.set([]);
     }
 };
